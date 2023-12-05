1fd23f9de320ba9a0ba9e7430e05c571b3a5369f
waltscode <walterswork91@outlook.com>
2023-12-05 15:40:46 -0500

    added cards functionality by cuisine type

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -1,148 +1,94 @@
-// const axios = require('axios');
-
-// const apiKey = '0344dc0b28eb42d9b03d968ad5c30085';
-// const cuisine = 'Greek';
-
-// const fetchGreekRecipes = async () => {
-//   try {
-//     const response = await axios.get(
-//       `https://api.spoonacular.com/recipes/complexSearch?cuisine=${cuisine}&apiKey=${apiKey}`
-//     );
-
-//     const recipes = response.data.results;
-//     console.log('Greek Recipes:', recipes);
-//   } catch (error) {
-//     console.error('Error fetching Greek recipes:', error.message);
-//   }
-// };
-
-// fetchGreekRecipes();
-// const axios = require('axios');
-
-// const apiKey = '0344dc0b28eb42d9b03d968ad5c30085';
-// const cuisines = [
-//   'African', 'Asian', 'American', 'British', 'Cajun', 'Caribbean',
-//   'Chinese', 'Eastern European', 'European', 'French', 'German', 'Greek',
-//   'Indian', 'Irish', 'Italian', 'Japanese', 'Jewish', 'Korean',
-//   'Latin American', 'Mediterranean', 'Mexican', 'Middle Eastern',
-//   'Nordic', 'Southern', 'Spanish', 'Thai', 'Vietnamese'
-// ];
-
-// const fetchRecipesByCuisine = async () => {
-//   try {
-//     for (const cuisine of cuisines) {
-//       const response = await axios.get(
-//         `https://api.spoonacular.com/recipes/complexSearch?cuisine=${cuisine}&apiKey=${apiKey}`
-//       );
-
-//       const recipes = response.data.results;
-//       console.log(`${cuisine} Recipes:`, recipes);
-//     }
-//   } catch (error) {
-//     console.error('Error fetching recipes:', error.message);
-//   }
-// };
-
-// fetchRecipesByCuisine();
-
-// const apiKey = 'a5d7286f6b864fef82d1dd50060278cb';
-// const cuisines = [
-//   'African', 'Asian', 'American', 'British', 'Cajun', 'Caribbean',
-//   'Chinese', 'Eastern European', 'European', 'French', 'German', 'Greek',
-//   'Indian', 'Irish', 'Italian', 'Japanese', 'Jewish', 'Korean',
-//   'Latin American', 'Mediterranean', 'Mexican', 'Middle Eastern',
-//   'Nordic', 'Southern', 'Spanish', 'Thai', 'Vietnamese'
-// ];
-
-// const fetchRecipesByCuisine = async () => {
-//   try {
-//     for (const cuisine of cuisines) {
-//       const response = await fetch(
-//         `https://api.spoonacular.com/recipes/complexSearch?cuisine=${cuisine}&apiKey=${apiKey}`
-//       );
-
-//       if (!response.ok) {
-//         throw new Error(`HTTP error! Status: ${response.status}`);
-//       }
-
-//       const data = await response.json();
-//       const recipes = data.results;
-//       console.log(`${cuisine} Recipes:`, recipes);
-     
-//     }
-//   } catch (error) {
-//     console.error('Error fetching recipes:', error.message);
-//   }
-// };
-
-// fetchRecipesByCuisine();
 const appId = 'e2556156';
 const appKey = 'ef1fc19b395ec4ed7dd6206da381fc0f';
 const query = 'cuisines';
 
+// Use getElementsByClassName or querySelector with the correct class selector
+const cardContainer = document.getElementsByClassName('card-container')[0];
+
 async function fetchRecipesByCuisine(cuisine) {
-    const url = `https://api.edamam.com/search?q=${cuisine}&app_id=${appId}&app_key=${appKey}`;
-  
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-  
-      // Check if recipes exist in the response
-      if (data.hits && data.hits.length > 0) {
-        // Log details of recipes within the chosen cuisine
-        console.log(`Recipes for ${cuisine}:`);
-        data.hits.forEach(recipe => {
-          console.log('Label:', recipe.recipe.label);
-          console.log('Ingredients:', recipe.recipe.ingredientLines);
-          console.log('URL:', recipe.recipe.url);
-          console.log('---------------------');
-        });
-      } else {
-        console.log(`No recipes found for ${cuisine}.`);
-      }
-    } catch (error) {
-      console.error(error);
+  // Set the 'to' parameter to fetch more than 10 items
+  const url = `https://api.edamam.com/search?q=${cuisine}&app_id=${appId}&app_key=${appKey}&to=30`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    // Check if recipes exist in the response
+    if (data.hits && data.hits.length > 0) {
+      // Clear previous cards
+      cardContainer.innerHTML = '';
+
+      // Create and append cards for each recipe
+      data.hits.forEach(recipe => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.style = 'width: 18rem;';
+
+        const img = document.createElement('img');
+        img.src = recipe.recipe.image;
+        img.className = 'card-img-top';
+        img.alt = recipe.recipe.label;
+
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+
+        const cardText = document.createElement('p');
+        cardText.className = 'card-text';
+        cardText.textContent = recipe.recipe.label;
+
+        cardBody.appendChild(cardText);
+        card.appendChild(img);
+        card.appendChild(cardBody);
+
+        // Append the card to the container
+        cardContainer.appendChild(card);
+      });
+    } else {
+      console.log(`No recipes found for ${cuisine}.`);
     }
+  } catch (error) {
+    console.error(error);
   }
-  
-  async function fetchCuisineCategories() {
-    try {
-      const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`);
-      const data = await response.json();
-  
-      // Check if recipes exist in the response
-      if (data.hits && data.hits.length > 0) {
-        // Collect unique cuisine types
-        const cuisineTypesSet = new Set();
-        data.hits.forEach(recipe => {
-          if (recipe.recipe.cuisineType) {
-            recipe.recipe.cuisineType.forEach(cuisine => {
-              cuisineTypesSet.add(cuisine);
-            });
-          }
-        });
-  
-        // Convert set to array for creating buttons
-        const cuisineTypesArray = [...cuisineTypesSet];
-  
-        // Create buttons for each cuisine category
-        cuisineTypesArray.forEach(cuisine => {
-          const button = document.createElement('button');
-          button.textContent = cuisine;
-          button.addEventListener('click', () => {
-            // Fetch and log recipes for the chosen cuisine
-            fetchRecipesByCuisine(cuisine);
+}
+
+async function fetchCuisineCategories() {
+  try {
+    const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${appId}&app_key=${appKey}`);
+    const data = await response.json();
+
+    // Check if recipes exist in the response
+    if (data.hits && data.hits.length > 0) {
+      // Collect unique cuisine types
+      const cuisineTypesSet = new Set();
+      data.hits.forEach(recipe => {
+        if (recipe.recipe.cuisineType) {
+          recipe.recipe.cuisineType.forEach(cuisine => {
+            cuisineTypesSet.add(cuisine);
           });
-  
-          // Append the button to a container element (e.g., body)
-          document.body.appendChild(button);
+        }
+      });
+
+      // Convert set to array for creating buttons
+      const cuisineTypesArray = [...cuisineTypesSet];
+
+      // Create buttons for each cuisine category
+      cuisineTypesArray.forEach(cuisine => {
+        const button = document.createElement('button');
+        button.textContent = cuisine;
+        button.addEventListener('click', () => {
+          // Fetch and display recipes for the chosen cuisine
+          fetchRecipesByCuisine(cuisine);
         });
-      } else {
-        console.log('No recipes found.');
-      }
-    } catch (error) {
-      console.error(error);
+
+        // Append the button to the container element (e.g., body)
+        document.body.appendChild(button);
+      });
+    } else {
+      console.log('No recipes found.');
     }
+  } catch (error) {
+    console.error(error);
   }
-  
-  fetchCuisineCategories();
+}
+
+fetchCuisineCategories();
