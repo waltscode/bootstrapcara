08b9e53401d30e8cb66d34c50d6184e3944c7be8
waltscode <walterswork91@outlook.com>
2023-12-05 16:29:05 -0500

    added intital functionality and layout of cuisine cards and buttons

--- assets/js/styles.js
+++ assets/js/styles.js
@@ -3,10 +3,11 @@ const appKey = 'ef1fc19b395ec4ed7dd6206da381fc0f';
 const query = 'cuisines';
 
 // Use getElementsByClassName or querySelector with the correct class selector
-const cardContainer = document.getElementsByClassName('card-container')[0];
+// Use getElementsByClassName or querySelector with the correct class selector
+const choicesContainer = document.querySelector('.cuisine-choices');
+const cardContainer = document.querySelector('.card-container');
 
 async function fetchRecipesByCuisine(cuisine) {
-  // Set the 'to' parameter to fetch more than 10 items
   const url = `https://api.edamam.com/search?q=${cuisine}&app_id=${appId}&app_key=${appKey}&to=30`;
 
   try {
@@ -15,32 +16,15 @@ async function fetchRecipesByCuisine(cuisine) {
 
     // Check if recipes exist in the response
     if (data.hits && data.hits.length > 0) {
+      // Randomize the order of recipes
+      const randomizedRecipes = shuffleArray(data.hits);
+
       // Clear previous cards
       cardContainer.innerHTML = '';
 
-      // Create and append cards for each recipe
-      data.hits.forEach(recipe => {
-        const card = document.createElement('div');
-        card.className = 'card';
-        card.style = 'width: 18rem;';
-
-        const img = document.createElement('img');
-        img.src = recipe.recipe.image;
-        img.className = 'card-img-top';
-        img.alt = recipe.recipe.label;
-
-        const cardBody = document.createElement('div');
-        cardBody.className = 'card-body';
-
-        const cardText = document.createElement('p');
-        cardText.className = 'card-text';
-        cardText.textContent = recipe.recipe.label;
-
-        cardBody.appendChild(cardText);
-        card.appendChild(img);
-        card.appendChild(cardBody);
-
-        // Append the card to the container
+      // Create and append cards for each randomized recipe
+      randomizedRecipes.forEach(recipe => {
+        const card = createRecipeCard(recipe);
         cardContainer.appendChild(card);
       });
     } else {
@@ -49,6 +33,39 @@ async function fetchRecipesByCuisine(cuisine) {
   } catch (error) {
     console.error(error);
   }
+}
+
+function createRecipeCard(recipe) {
+  const card = document.createElement('div');
+  card.className = 'card';
+  card.style = 'width: 18rem;';
+
+  const img = document.createElement('img');
+  img.src = recipe.recipe.image;
+  img.className = 'card-img-top';
+  img.alt = recipe.recipe.label;
+
+  const cardBody = document.createElement('div');
+  cardBody.className = 'card-body';
+
+  const cardText = document.createElement('p');
+  cardText.className = 'card-text';
+  cardText.textContent = recipe.recipe.label;
+
+  cardBody.appendChild(cardText);
+  card.appendChild(img);
+  card.appendChild(cardBody);
+
+  return card;
+}
+
+function shuffleArray(array) {
+  // Fisher-Yates Shuffle Algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
 }
 
 async function fetchCuisineCategories() {
@@ -76,12 +93,12 @@ async function fetchCuisineCategories() {
         const button = document.createElement('button');
         button.textContent = cuisine;
         button.addEventListener('click', () => {
-          // Fetch and display recipes for the chosen cuisine
+          // Fetch and display a new set of randomized recipes for the chosen cuisine
           fetchRecipesByCuisine(cuisine);
         });
 
-        // Append the button to the container element (e.g., body)
-        document.body.appendChild(button);
+        // Append the button to the choices container
+        choicesContainer.appendChild(button);
       });
     } else {
       console.log('No recipes found.');
